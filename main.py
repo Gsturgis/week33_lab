c15c531505de48d22b8901fc935e79166c01742a
Gyasi Sturgis <gmac@Gyasis-Air.home>
2021-05-21 02:34:32 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from package import student
+def main():
+    print('hello')
+    student.name()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ package/student.py
@@ -0,0 +1,4 @@
+def name():
+    first = input("What is your first name? ")
+    last = input("what is your last name? ")
+    print(f"The students name is {last},{first}")
